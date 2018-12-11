100acfab8dc064aa3eed00c28dd36e2e091f2c65
probachaydmitry <dmytro.probachay@ria.com>
2018-12-11 18:38:11 +0200

    init np

--- NomeroffNet/filters.py
+++ NomeroffNet/filters.py
@@ -10,3 +10,16 @@ def mask(nns):
             gray = skimage.color.gray2rgb(mask) * 255
             res.append(gray)
     return res
+
+def color_splash(image, nns):
+    res = []
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 100
+    for nn in nns:
+        if nn["masks"].shape[-1] > 0:
+            mask = (np.sum(nn["masks"], -1, keepdims=True) >= 1)
+            fulled = np.full(image.shape, (255, 0, 0))
+            splash = np.where(mask, fulled, gray).astype(np.uint8)
+        else:
+            splash = gray.astype(np.uint8)
+        res.append(splash)
+    return res
--- examples/demo1.py
+++ examples/demo1.py
@@ -1,17 +1,40 @@
+# Import all necessary libraries.
 import sys
 import os
 import json
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
 
+# Load default configuration file.
 with open('../config/default.json') as data_file:
     config = json.load(data_file)
 
 sys.path.append(os.path.abspath(config["NOMEROFF_NET"]["ROOT"]))
 
+# Import license plate recognition tools.
 from NomeroffNet import Detector, filters
 
+# Initialize the detector with default configuration file.
 nnet = Detector(config)
-nnet.loadModel()
-np = nnet.detect(["/var/www/py/nomeroff-net/examples/images/250024318.jpeg"])
-masks = filters.mask(np)
 
-print(masks)
+# Load weights in keras format.
+nnet.loadModel()
+
+# Walking through the ./examples/images/ directory and checking each of the images for license plates.
+rootDir = 'images/'
+
+for dirName, subdirList, fileList in os.walk(rootDir):
+    for fname in fileList:
+        img_path = os.path.join(dirName, fname)
+
+        np = nnet.detect([img_path])
+
+        img = mpimg.imread(img_path)
+        plt.imshow(img)
+        plt.show()
+
+        # Generate masks.
+        masks = filters.mask(np)
+        for mask in masks:
+            plt.imshow(mask)
+            plt.show()
