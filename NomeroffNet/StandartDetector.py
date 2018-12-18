6802fa8bd43c31474f9763e15f6ff36c39dd35ca
probachaydmitry <dmytro.probachay@ria.com>
2018-12-18 15:38:32 +0200

    add Number Plate standart detector

---
+++ NomeroffNet/StandartDetector.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+class StandartDetector():
+    def __init__(self):
+        self.MODEL = None
+        self.CLASS_LABELS = ["euro", "ukr2015", "ukr2004"]
+
+    def load(self, path_to_model, verbose = 0):
+        self.MODEL = load_model(path_to_model)
+        if verbose:
+            self.MODEL.summary()
+
+    def getLabels(self, index):
+        return self.CLASS_LABELS[index]
+
+    def normalize(self, img):
+        img = img / 255.
+        img = cv2.resize(img, (150, 32))
+        return img
+
+    def predict(self, img):
+        predicted = self.MODEL.predict(np.array([img]))
+        return int(np.argmax(predicted))
