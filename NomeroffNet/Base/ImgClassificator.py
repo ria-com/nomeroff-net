5d84fe9cda15951251c1bb01a31c178b79b46479
probachaydmitry <dmytro.probachay@ria.com>
2019-01-18 17:05:33 +0200

    add train Example

--- NomeroffNet/Base/ImgClassificator.py
+++ NomeroffNet/Base/ImgClassificator.py
@@ -201,7 +201,7 @@ class ImgClassificator():
     def getLabels(self, index):
         return self.CLASS_LABELS[index]
 
-     def normalize(self, img):
+    def normalize(self, img):
         img = img / 255.
         img = cv2.resize(img, (295, 64))
         return img
