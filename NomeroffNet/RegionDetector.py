c13a5df792d030d604a54b22a06c97808392518f
probachaydmitry <dmytro.probachay@ria.com>
2019-01-18 14:55:39 +0200

    add Runtime test

--- NomeroffNet/RegionDetector.py
+++ NomeroffNet/RegionDetector.py
@@ -5,6 +5,10 @@ from .Base import ImgClassificator
 
 class RegionDetector(ImgClassificator):
     def __init__(self):
-        self.MODEL = None
-        self.CLASS_LABELS = ["xx-unknown", "eu-ua-2015", "eu-ua-2004", "eu-ua-1995", "eu", "xx-transit"]
         ImgClassificator.__init__(self)
+        # input
+        self.HEIGHT         = 64
+        self.WEIGHT         = 295
+
+        # outputs
+        self.CLASS_LABELS = ["xx-unknown", "eu-ua-2015", "eu-ua-2004", "eu-ua-1995", "eu", "xx-transit"]
