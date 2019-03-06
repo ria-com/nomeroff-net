f578556382f29436cbbab90dee8b84c5f088d0d6
Oleg Cherniy <oleg.cherniy@gmail.com>
2019-03-06 16:28:32 +0200

    Add processing time to onlinedemo form

--- public/npdetect.php
+++ public/npdetect.php
@@ -1,6 +1,6 @@
 <?php
     function detectNumberplate($filename) {
-        $data = array("doRecognizing" => 1, "image_url" => "https://nomeroff.net.ua/uploads/".$filename);
+        $data = array("doRecognizing" => 1, "includeReestrInfo" => 1, "image_url" => "https://nomeroff.net.ua/uploads/".$filename);
 
         $data_string = json_encode($data);
 
@@ -16,8 +16,37 @@
         return json_decode($result);
     }
 
+    function getRealIpAddr() {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  //to check ip is pass from proxy
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 
-    if ( 0 < $_FILES['file']['error'] ) {
+    function checkReCaptcha($token) {
+        $data = array("secret" => "6LdvT4sUAAAAABeNHOHJXaj-UE_CnpRRHkJBKGcB", "response" => $token); //, remoteip => getRealIpAddr()
+
+        $ch = curl_init('https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
+    }
+
+    // Init variables
+    $version = '1.7';
+    $reCaptcha = checkReCaptcha($_POST["token"]);
+    //$reCaptcha = [ 'success' => false ];
+
+    if ( !$reCaptcha->success ) {
+    //if ( false ) {
+        $data = [ 'error_id' => 4, success=>false, 'error_message' => 'reCapcha error: '.implode(",",$reCaptcha->{'error-codes'}) ]; 
+    } elseif ( 0 < $_FILES['file']['error'] ) {
         $data = [ 'error_id' => 1, success=>false, 'error_message' => $_FILES['file']['error'] ];
     } else {
         $file = $_FILES['file']['tmp_name'];
@@ -43,5 +72,7 @@
         }
     }
     header('Content-Type: application/json');
+    $data['reCaptcha'] = $reCaptcha;
+    $data['version'] = $version;
     echo json_encode($data);
 ?>
