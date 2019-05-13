2f109bc50fd13c1eb3f7ea4974d9fec2eff791ee
probachaydmitry <dmytro.probachay@ria.com>
2019-05-13 17:11:01 +0300

    spliter & 2line detector

---
+++ NomeroffNet/TextDetectors/2line.py
@@ -0,0 +1,7 @@
+from Base import OCR
+
+class eu(OCR):
+    def __init__(self):
+        OCR.__init__(self)
+        self.letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                                       "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
