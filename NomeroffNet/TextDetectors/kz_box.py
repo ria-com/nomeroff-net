cc342e01021dce95d8e05621456eb67ab696f2fe
probachaydmitry <dmytro.probachay@ria.com>
2019-05-22 17:40:02 +0300

    predict 3 output

--- NomeroffNet/TextDetectors/kz_box.py
+++
@@ -1,7 +0,0 @@
-from .box import box
-
-class kz_box(box):
-    def __init__(self):
-        box.__init__(self)
-        self.letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-                                       "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
