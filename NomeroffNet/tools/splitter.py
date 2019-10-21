60f74b765ac2715ea76673f1b5217108f311d31c
probachaydmitry <dmytro.probachay@ria.com>
2019-10-21 10:21:10 +0300

    fix requirements.txt for new Keras version

--- NomeroffNet/tools/splitter.py
+++ NomeroffNet/tools/splitter.py
@@ -15,3 +15,19 @@ def np_split(imgs, lines, coef=0.1):
             lastPart  = img[-n:]
             res_imgs.append(numpy.concatenate((firstPart, lastPart), axis=1))
     return numpy.array(res_imgs)
+
+
+def np_split_sorce(imgs, lines, coef=0.1):
+    res_imgs = []
+    for img, line in zip(imgs, lines):
+        if line < 2:
+            res_imgs.append(img)
+        else:
+            n = int(img.shape[0]/line + (img.shape[0] * coef))
+            firstPart = img[:n]
+            for l in range(1, line - 1):
+                middlePart = img[:l*n]
+                firstPart  = numpy.concatenate((firstPart, middlePart), axis=1)
+            lastPart  = img[-n:]
+            res_imgs.append([firstPart, lastPart])
+    return numpy.array(res_imgs)
