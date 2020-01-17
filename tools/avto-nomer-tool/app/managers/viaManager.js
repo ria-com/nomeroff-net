185b4de95c27ee2dfae61ba0383a3a8e77760c2d
Oleg Cherniy <oleg.cherniy@gmail.com>
2020-01-17 16:58:21 +0200

    Update some train scripts & nuberplate dataset tools

--- tools/avto-nomer-tool/app/managers/viaManager.js
+++ tools/avto-nomer-tool/app/managers/viaManager.js
@@ -9,21 +9,26 @@ module.exports = {
         // console.log('keys-----------------------------');
         // console.log(keys);
         for (let item of keys) {
-            let file =  path.join(srcDir, item);
+            //console.log(`item: ${item}`);
+            let file =  path.join(srcDir, data._via_img_metadata[item].filename);
+            //console.log(file);
             if (fs.existsSync(file)) {
                 dataPart._via_img_metadata[item] = data._via_img_metadata[item]
             } else {
                 new Error(`File "${item}" is not exists!`)
             }
         }
+        //console.log(dataPart);
         return dataPart
     },
 
     moveViaPart(dataPart, srcDir, targetDir, subDir) {
+        //console.log(`Start moving ${targetDir}`);
         for (let key in dataPart._via_img_metadata) {
             let fileIn = path.join(srcDir, dataPart._via_img_metadata[key].filename),
                 fileOut = path.join(targetDir, subDir, dataPart._via_img_metadata[key].filename)
             ;
+            //console.log(`fileIn: ${fileIn}\nfileOut: ${fileOut}`);
             if (!fs.existsSync(fileOut)) {
                 fs.renameSync(fileIn, fileOut);
             }
@@ -33,6 +38,10 @@ module.exports = {
 
     writeViaPart(dataPart, targetDir, subDir, viaFile) {
         let fullViaFile = path.join(targetDir, subDir, viaFile);
+        module.exports.writeViaPartFull(dataPart, fullViaFile);
+    },
+
+    writeViaPartFull(dataPart, fullViaFile) {
         let wstream = fs.createWriteStream(fullViaFile);
         wstream.write(JSON.stringify(dataPart,null,2));
         wstream.end();
--- tools/avto-nomer-tool/app_console/controllers/viaController.js
+++ tools/avto-nomer-tool/app_console/controllers/viaController.js
@@ -4,8 +4,9 @@ const config = require('config'),
     checkDir = require('../../app/helpers/checkDir'),
     checkDirStructure = require('../../app/helpers/checkDirStructure'),
     arrayShuffle = require('../../app/helpers/arrayShuffle'),
+    arrayGroupFix = require('../../app/helpers/arrayGroupFix'),
     sleep = require('sleep-promise'),
-    {prepareViaPart,moveViaPart,writeViaPart} = require('../../app/managers/viaManager')
+    {prepareViaPart,moveViaPart,writeViaPart,writeViaPartFull} = require('../../app/managers/viaManager')
 ;
 
 /**
@@ -84,5 +85,89 @@ async function joinVia (options) {
     await sleep(1000)
 };
 
+/**
+ * Объеденить 2 датасета в один для Mask RCNN
+ *
+ * @param options
+ * @example ./console.js --section=via --action=addAttribute --opt.srcJson=/mnt/data/home/nn/datasets/autoriaNumberplateDataset-2019-09-17/emptyPlate/via_region_data_emptyPlate.json --opt.attrName=class --opt.attrValue=emptyPlate
+ */
+async function addAttribute (options) {
+    //console.log('Hello world defaultController & index action with options: ' +JSON.stringify(options));
+    const srcJson = options.srcJson || new Error('"opt.srcJson" is not defined!') ,
+        attrName = options.attrName || new Error('"opt.attrName" is not defined!'),
+        attrValue = options.attrValue || new Error('"opt.attrName" is not defined!')
+    ;
+    let srcDir = path.dirname(srcJson);
+    checkDir(srcDir);
+    let dataPart = require(srcJson), data = dataPart._via_img_metadata;
+    for (let key in data) {
+        let item = data[key];
+        for (let region of item.regions) {
+            region.region_attributes[attrName] = attrValue
+        }
+    }
+    writeViaPartFull(dataPart,srcJson);
+    await sleep(1000)
+};
 
-module.exports = {index, split, joinVia};
+function convertToViaIdx(viaIdx,arr) {
+    let newArr = [];
+    for(let idx of arr) {
+        newArr.push(viaIdx[idx])
+    }
+    return newArr;
+}
+
+/**
+ * Создание val i train для Mask RCNN
+ *
+ * @param options
+ * @example ./console.js --section=via --action=groupSplit --opr.rate=0.2 --opt.srcDir=/mnt/data/home/nn/datasets/autoriaNumberplateDataset-2019-06-07/draft --opt.targetDir=/mnt/data/home/nn/datasets/autoriaNumberplateDataset-2019-06-07 --opt.viaFile=via_region_data.json
+ */
+async function groupSplit (options) {
+    const srcDir = options.srcDir || new Error('"opt.srcDir" is not defined!'), // /mnt/data/home/nn/datasets/autoriaNumberplateDataset-2019-06-07/draft,
+        targetDir = options.targetDir || new Error('"opt.targetDir" is not defined!'), // /mnt/data/home/nn/datasets/autoriaNumberplateDataset-2019-06-07,
+        viaFile = options.viaFile || new Error('"opt.viaFile" is not defined!'), // via_region_data.json,
+        splitRate = options.rate || 0.2,
+        srcViaPath = path.join(srcDir, viaFile),
+        data = require(srcViaPath),
+        viaIdx = {}
+    ;
+    for (let via_idx in data._via_img_metadata) {
+        viaIdx[data._via_img_metadata[via_idx].filename] = via_idx;
+    }
+
+    const keys = arrayShuffle(Object.keys(viaIdx))
+        cnt = Math.round(keys.length * (1-splitRate)),
+        partKeys = arrayGroupFix((1-splitRate), {
+            'train': keys.slice(cnt),
+            'val': keys.slice(0,cnt)
+        })
+    ;
+
+    partKeys.train =  convertToViaIdx(viaIdx,partKeys.train);
+    partKeys.val =  convertToViaIdx(viaIdx,partKeys.val);
+
+    // console.log(`-----------------------------partKeys.train`);
+    // console.log(partKeys.train);
+    // console.log(`-----------------------------partKeys.val`);
+    // console.log(partKeys.val);
+
+    checkDir(srcDir);
+    checkDirStructure(targetDir,config.via.partDirs,true);
+
+    for (let key of config.via.partDirs) {
+        //checkDir(path.join(targetDir, key), true);
+        let dataPart = prepareViaPart(data, partKeys[key], srcDir);
+        //console.log(`making dataPart for ${key} is done...`)
+        moveViaPart(dataPart,srcDir,targetDir,key);
+        console.log(`moveViaPart for ${key} is done...`)
+        writeViaPart(dataPart,targetDir,key,viaFile);
+        console.log(`Macking ${key} done...`)
+    }
+    console.log(`Final rate: ${partKeys.val.length/(partKeys.train.length+partKeys.val.length)}`);
+    await sleep(1000)
+};
+
+
+module.exports = {index, split, joinVia, addAttribute, groupSplit};
