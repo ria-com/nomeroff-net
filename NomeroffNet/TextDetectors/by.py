939f3829576058f8b567b453a8be54528bfa6aa6
Serhii Hladiholov <serhii.hladiholov@ria.com>
2020-11-13 14:06:25 +0200

    Added "by" TextDetector

--- NomeroffNet/TextDetectors/by.py
+++ NomeroffNet/TextDetectors/by.py
@@ -1,11 +1,11 @@
 from Base import OCR
 
 
-class eu_ua_2004_2015(OCR):
+class eu(OCR):
     def __init__(self):
         OCR.__init__(self)
-        self.letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "E", "H", "I", "K", "M",
-                        "O", "P", "T", "X"]
+        self.letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'E', 'H', 'I',
+                        'K', 'M', 'O', 'P', 'T', 'X']
         self.max_text_len = 8
         self.letters_max = len(self.letters)+1
         self.label_length = 32 - 2
--- NomeroffNet/TextDetectors/ge.py
+++ NomeroffNet/TextDetectors/ge.py
@@ -1,9 +1,11 @@
 from Base import OCR
 
+
 class ge(OCR):
     def __init__(self):
         OCR.__init__(self)
-        self.letters      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        self.letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                        "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         self.max_text_len = 8
-        self.letters_max  = len(self.letters)+1
+        self.letters_max = len(self.letters)+1
         self.label_length = 32 - 2
