080a66139ec3bed7ba1de6ac5c9818a1212bbce2
dmitroprobachay <dmytro.probachay@ria.com>
2021-05-25 14:59:43 +0300

    Repair textPostprocessing

--- NomeroffNet/TextPostprocessing.py
+++ NomeroffNet/TextPostprocessing.py
@@ -30,9 +30,8 @@ def textPostprocessing(texts: List[str], text_postprocess_name: List[str]) -> Li
     for text, textPostprocessName in zip(texts, text_postprocess_name):
         _textPostprocessName = textPostprocessName.replace("-", "_")
         if _textPostprocessName in dir(TextPostprocessings):
-            text_postprocessing = getattr(getattr(TextPostprocessings, _textPostprocessName), _textPostprocessName)
+            postprocess_manager = getattr(getattr(TextPostprocessings, _textPostprocessName), _textPostprocessName)
         else:
-            text_postprocessing = getattr(getattr(TextPostprocessings, "xx_xx"), "xx_xx")
-        postprocess_manager = text_postprocessing()
+            postprocess_manager = getattr(getattr(TextPostprocessings, "xx_xx"), "xx_xx")
         res_texts.append(postprocess_manager.find(text))
     return res_texts
