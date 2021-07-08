fdb78f522b09de2e7b63d10107fe2b3fe4a1d2ad
dmitroprobachay <dmytro.probachay@ria.com>
2021-07-08 20:43:39 +0300

    Update options accuracy log

--- NomeroffNet/nnmodels/numberplate_options_model.py
+++ NomeroffNet/nnmodels/numberplate_options_model.py
@@ -87,19 +87,28 @@ class NPOptionsNet(pl.LightningModule):
         loss, acc = self.step(batch)
         self.log(f'Batch {batch_idx} train_loss', loss)
         self.log(f'Batch {batch_idx} accuracy', acc)
-        return loss
+        return {
+            'loss': loss,
+            'acc': acc,
+        }
 
     def validation_step(self, batch, batch_idx):
         loss, acc = self.step(batch)
         self.log('val_loss', loss)
         self.log(f'val_accuracy', acc)
-        return loss
+        return {
+            'val_loss': loss,
+            'val_acc': acc,
+        }
 
     def test_step(self, batch, batch_idx):
         loss, acc = self.step(batch)
         self.log('test_loss', loss)
         self.log(f'test_accuracy', acc)
-        return loss
+        return {
+            'test_loss': loss,
+            'test_acc': acc,
+        }
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adamax(self.parameters(),
