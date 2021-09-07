289f24abb53fe728167a75c9d8fa8a88e11fcb55
dmitroprobachay <dmytro.probachay@ria.com>
2021-09-07 18:59:51 +0300

    Repair newest Pillow version bug

--- NomeroffNet/MultiLineNPExtractor.py
+++ NomeroffNet/MultiLineNPExtractor.py
@@ -5,7 +5,6 @@ import numpy as np
 from typing import List, Dict, Tuple
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
 from tools import (fline,
                    distance,
                    minimum_bounding_rectangle,
