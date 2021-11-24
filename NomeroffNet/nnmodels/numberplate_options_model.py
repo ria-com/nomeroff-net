f8178c07d83ef408575f212119fc5a3b2a509cce
dmitroprobachay <dmytro.probachay@ria.com>
2021-11-24 11:58:54 +0200

    Added resnet18 backend to option detector

--- NomeroffNet/nnmodels/numberplate_options_model.py
+++ NomeroffNet/nnmodels/numberplate_options_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional
 from .classification_model import ClassificationNet
+from torchvision.models import resnet18
 
 
 class NPOptionsNet(ClassificationNet):
@@ -15,31 +16,19 @@ class NPOptionsNet(ClassificationNet):
         super(NPOptionsNet, self).__init__()  # activation='relu'
         self.batch_size = batch_size
         self.learning_rate = learning_rate
-        self.inp_conv = nn.Conv2d(3, 32, (3, 3),
-                                  stride=(1, 1),
-                                  padding=(0, 0))
-        self.conv1 = nn.Conv2d(32, 64, (3, 3),
-                               stride=(1, 1),
-                               padding=(0, 0))
-        self.conv2 = nn.Conv2d(64, 128, (3, 3),
-                               stride=(1, 1),
-                               padding=(0, 0))
-        self.conv3 = nn.Conv2d(128, 128, (3, 3),
-                               stride=(1, 1),
-                               padding=(0, 0))
-        self.pool = nn.MaxPool2d(2, 2)
 
-        img_w = int(img_w / 2 / 2 / 2 / 2 - 2)
-        img_h = int(img_h / 2 / 2 / 2 / 2 - 2)
+        resnet = resnet18(pretrained=True)
+        modules = list(resnet.children())[:-3]
+        self.resnet = nn.Sequential(*modules)
 
         self.dropout_reg = nn.Dropout(0.2)
-        self.fc1_reg = nn.Linear(128 * img_w * img_h, 512)
+        self.fc1_reg = nn.Linear(256 * 4 * 19, 512)
         self.fc2_reg = nn.Linear(512, 256)
         self.batch_norm_reg = nn.BatchNorm1d(512)
         self.fc3_reg = nn.Linear(256, region_output_size)
-        
+
         self.dropout_line = nn.Dropout(0.2)
-        self.fc1_line = nn.Linear(128 * img_w * img_h, 512)
+        self.fc1_line = nn.Linear(256 * 4 * 19, 512)
         self.fc2_line = nn.Linear(512, 256)
         self.batch_norm_line = nn.BatchNorm1d(512)
         self.fc3_line = nn.Linear(256, count_line_output_size)
@@ -65,10 +54,10 @@ class NPOptionsNet(ClassificationNet):
 
     def validation_step(self, batch, batch_idx):
         loss, acc, acc_reg, acc_line = self.step(batch)
-        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log(f'val_accuracy', acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('val_acc_reg', acc_reg, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log(f'val_acc_line', acc_line, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f'val_accuracy', acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_acc_reg', acc_reg, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f'val_acc_line', acc_line, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         tqdm_dict = {
             'val_loss': loss,
             'acc': acc,
@@ -83,10 +72,10 @@ class NPOptionsNet(ClassificationNet):
 
     def test_step(self, batch, batch_idx):
         loss, acc, acc_reg, acc_line = self.step(batch)
-        self.log('test_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log(f'test_accuracy', acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('test_acc_reg', acc_reg, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log(f'test_acc_line', acc_line, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f'test_accuracy', acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('test_acc_reg', acc_reg, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f'test_acc_line', acc_line, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         tqdm_dict = {
             'test_loss': loss,
             'acc': acc,
@@ -100,11 +89,8 @@ class NPOptionsNet(ClassificationNet):
         }
 
     def forward(self, x):
-        x = self.pool(functional.relu(self.inp_conv(x)))
-        x = self.pool(functional.relu(self.conv1(x)))
-        x = self.pool(functional.relu(self.conv2(x)))
-        x = self.pool(functional.relu(self.conv3(x)))
-        
+        x = self.resnet(x)
+
         x1 = x.reshape(x.size(0), -1)
         x1 = self.dropout_reg(x1)
         x1 = functional.relu(self.fc1_reg(x1))
@@ -112,7 +98,7 @@ class NPOptionsNet(ClassificationNet):
             x1 = self.batch_norm_reg(x1)
         x1 = functional.relu(self.fc2_reg(x1))
         x1 = functional.softmax(self.fc3_reg(x1))
-        
+
         x2 = x.reshape(x.size(0), -1)
         x2 = self.dropout_line(x2)
         x2 = functional.relu(self.fc1_line(x2))
@@ -141,12 +127,10 @@ class NPOptionsNet(ClassificationNet):
         return loss, acc, acc_reg, acc_line
 
     def configure_optimizers(self):
-        # optimizer = torch.optim.Adamax(self.parameters(),
-        #                                lr=self.learning_rate,
-        #                                betas=(0.9, 0.999),
-        #                                eps=1e-07)
-        # optimizer = torch.optim.SGD(self.parameters(),
-        #                                lr=self.learning_rate,
-        #                                momentum=0.9)
-        optimizer = torch.optim.ASGD(self.parameters(), lr=self.learning_rate, lambd=0.0001, alpha=0.75, t0=1000000.0, weight_decay=0)
+        optimizer = torch.optim.ASGD(self.parameters(),
+                                     lr=self.learning_rate,
+                                     lambd=0.0001,
+                                     alpha=0.75,
+                                     t0=1000000.0,
+                                     weight_decay=0)
         return optimizer
