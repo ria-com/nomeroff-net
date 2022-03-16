d3f1d81155731fb1e83fb8ed0780af75fd59f8c7
dmitroprobachay <dmytro.probachay@ria.com>
2022-03-16 16:28:27 +0200

    structured python and jupyter examples

--- examples/py/benchmark/accuracy-test.py
+++ examples/py/benchmark/accuracy-test.py
@@ -1,7 +1,7 @@
 import os
 import warnings
 from glob import glob
-from _paths import current_dir
+from _paths import nomeroff_net_dir
 
 from nomeroff_net import pipeline
 from nomeroff_net.tools import unzip
@@ -12,7 +12,8 @@ warnings.filterwarnings("ignore")
 if __name__ == '__main__':
     number_plate_detection_and_reading = pipeline("number_plate_detection_and_reading", image_loader="opencv")
 
-    image_paths = glob(os.path.join(current_dir, "../images/*"))
+    image_paths = glob(os.path.join(nomeroff_net_dir, "./data/examples/benchmark_oneline_np_images/1.jpeg"))
+    print(image_paths)
     result = number_plate_detection_and_reading(image_paths, quality_profile=[3, 1, 0])
 
     (images, images_bboxs,
@@ -21,7 +22,7 @@ if __name__ == '__main__':
      confidences, texts) = unzip(result)
 
     number_plate_detection_and_reading.text_accuracy_test_from_file(
-        os.path.join(current_dir, "../accuracy_test_data.json"),
+        os.path.join(nomeroff_net_dir, "./data/examples/accuracy_test_data_example.json"),
         texts, image_paths,
         images, images_bboxs,
         images_points, images_zones,
---
+++ nomeroff_net/tools/datasets_tools.py
@@ -0,0 +1,158 @@
+import json
+import tqdm
+import cv2
+import os
+import glob
+import shutil
+import pandas as pd
+import numpy as np
+import matplotlib.image as mpimg
+from nomeroff_net.pipes.number_plate_text_readers.text_detector import TextDetector
+from nomeroff_net.pipes.number_plate_localizators.yolo_v5_detector import Detector
+from nomeroff_net.pipes.number_plate_keypoints_detectors.bbox_np_points import NpPointsCraft
+
+from nomeroff_net.tools.image_processing import (
+    get_cv_zone_rgb,
+    convert_cv_zones_rgb_to_bgr,
+    reshape_points)
+
+
+def auto_number_grab(root_dir, csv_dataset_path, res_dir):
+    detector = Detector()
+    detector.load()
+
+    np_points_craft = NpPointsCraft()
+    np_points_craft.load()
+
+    photos = pd.read_csv(csv_dataset_path)
+    photos = photos.set_index(['photoId'])
+    if os.path.exists(res_dir):
+        shutil.rmtree(res_dir)
+    os.makedirs(res_dir, exist_ok=False)
+
+    for dir_name, subdir_list, file_list in os.walk(root_dir):
+        for fname in file_list:
+            baseName = os.path.splitext(os.path.basename(fname))[0]
+            img_path = os.path.join(dir_name, fname)
+            try:
+                img = mpimg.imread(img_path)
+            except IOError:
+                print(f"[WARNING] {img_path} is not image")
+                continue
+
+            target_boxes = detector.detect_bbox(img)
+
+            images_points, images_mline_boxes = np_points_craft.detect([[img, target_boxes]])
+            all_points = images_points[0]
+            all_points = [ps for ps in all_points if len(ps)]
+
+            # cut zones
+            to_show_zones = [get_cv_zone_rgb(img, reshape_points(rect, 1)) for rect in all_points]
+            zones = convert_cv_zones_rgb_to_bgr(to_show_zones)
+
+            if len(to_show_zones) > 1:
+                areas = [zone.shape[0] * zone.shape[1] for zone in zones]
+                zone = to_show_zones[np.argmax(areas)]
+            else:
+                zone = to_show_zones[0]
+
+            text = photos.loc[baseName]['npText']
+
+            imgpath = os.path.join(res_dir, "./{}_{}.png".format(baseName, text))
+            mpimg.imsave(imgpath, zone)
+
+
+def delete_not_used_images_from_via_dataset(
+        path_to_images,
+        path_to_json,
+        delete_not_used_images=True):
+    with open(path_to_json) as ann:
+        ann_data = json.load(ann)
+    image_list = ann_data
+
+    file_names = []
+    for _id in tqdm.tqdm(image_list["_via_img_metadata"]):
+        image_id = image_list["_via_img_metadata"][_id]["filename"]
+        file_name = f'{path_to_images}/{image_id}'
+
+        if not os.path.exists(file_name):
+            print("[PROBLEM] Path", file_name, "not exists")
+        file_names.append(file_name)
+    print("[INFO] json file", path_to_json, "has", len(file_names), "annotated items")
+
+    if not delete_not_used_images:
+        return 0
+    deleted_count = 0
+    for file_name in glob.glob(f"{path_to_images}/*"):
+        if file_name not in file_names and file_name != path_to_json:
+            os.remove(file_name)
+            deleted_count += 1
+    print("[INFO] deleted not used images ", deleted_count)
+    return 0
+
+
+def check_ocr_model(root_dir,
+                    model_path="latest",
+                    text_detector_name="eu",
+                    img_format="png",
+                    predicted_part_size=1000,
+                    replace_tamplate = None):
+    if replace_tamplate is None:
+        replace_tamplate = {'moderation': {'isModerated': 1, 'moderatedBy': 'ApelSYN'}}
+    text_detector = TextDetector({
+        text_detector_name: {
+            "for_regions": [""],
+            "model_path": model_path
+        },
+    })
+
+    ann_dir = os.path.join(root_dir, "ann")
+    jsons = []
+    jsons_paths = []
+    for dir_name, subdir_list, file_list in os.walk(ann_dir):
+        for fname in file_list:
+            fname = os.path.join(ann_dir, fname)
+            jsons_paths.append(fname)
+            with open(fname) as jsonF:
+                jsonData = json.load(jsonF)
+            jsons.append(jsonData)
+    print("LOADED {} ANNOTATIONS".format(len(jsons)))
+
+    img_dir = os.path.join(root_dir, "img")
+    imgs = []
+    for j in jsons:
+        img_path =os.path.join(img_dir, "{}.{}".format(j["name"], img_format))
+        img = cv2.imread(img_path)
+        imgs.append(img)
+    print("LOADED {} IMAGES".format(len(imgs)))
+
+    predicted = []
+    N = int(len(imgs) / predicted_part_size) + 1
+    for i in range(N):
+        part = i*predicted_part_size
+        part_imgs = imgs[part:part+predicted_part_size]
+        model_inputs = text_detector.preprocess(part_imgs, ["" for _ in part_imgs], [1 for _ in part_imgs])
+        model_outputs = text_detector.forward(model_inputs)
+        predicted_part = text_detector.postprocess(model_outputs)
+        predicted += predicted_part
+
+    print("PREDICTED {} IMAGES".format(len(predicted)))
+
+    err_cnt = 0
+    for i in range(len(jsons_paths)):
+        json_path      = jsons_paths[i]
+        predicted_item = predicted[i]
+        jsonData       = jsons[i]
+        jsonData["moderation"]["predicted"] = predicted_item
+        if jsonData["description"] == jsonData["moderation"]["predicted"]:
+            jsonData.update(replace_tamplate)
+            jsonData["moderation"]["isModerated"] = 1
+        else:
+            print("Predicted '{}', real: '{}' in file {}".format(jsonData["moderation"]["predicted"],jsonData["description"], json_path))
+            err_cnt = err_cnt+1
+
+        with open(json_path, "w") as jsonWF:
+            json.dump(jsonData, jsonWF)
+
+    print("Error detection count: {}".format(err_cnt))
+    print("Accuracy: {}".format(1-err_cnt/len(predicted)))
