f81068ca6c104a02798dc1710f5c6063ef1b7589
master <oleg.cherniy@gmail.com>
2022-07-26 10:28:02 +0300

    Add yolov6 detector

--- nomeroff_net/tools/mcm.py
+++ nomeroff_net/tools/mcm.py
@@ -22,9 +22,10 @@ model_config_urls = [
     "http://models.vsp.net.ua/config_model/nomeroff-net-ocr-md/model-1.json",
 
     # object detection
-    "http://models.vsp.net.ua/config_model/nomeroff-net-yolov5/model-1.json",
+    "http://models.vsp.net.ua/config_model/nomeroff-net-yolov5/model-2.json",
     "http://models.vsp.net.ua/config_model/nomeroff-net-yolox/model-1.json",
     "http://models.vsp.net.ua/config_model/nomeroff-net-yolov5_brand_np/model-1.json",
+    "http://models.vsp.net.ua/config_model/nomeroff-net-yolov6/model-1.json",
 
     # text localization
     "http://models.vsp.net.ua/config_model/craft-mlt/model-1.json",
