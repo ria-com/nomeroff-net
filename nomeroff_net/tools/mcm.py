d62b08b426465db6a45f86257069b073d4edd71b
master <oleg.cherniy@gmail.com>
2023-01-24 11:37:08 +0200

    Fix syntax 'PRESET' variable

--- nomeroff_net/tools/mcm.py
+++ nomeroff_net/tools/mcm.py
@@ -17,7 +17,7 @@ model_config_urls = [
     "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu/model-3.json",
     "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu/model-shufflenet_v2_x2_0-3.json",
 
-    "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu_ua_1995/model-4.json",
+    "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu_ua_1995/model-5.json",
     "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu_ua_1995/model-shufflenet_v2_x2_0-3.json",
 
     "https://models.vsp.net.ua/config_model/nomeroff-net-ocr-eu_ua_from_2004/model-8.json",
